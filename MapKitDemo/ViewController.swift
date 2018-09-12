d0dd34d44e074d7bd75fb776443d0b56bcb19e3b
Florian Praile <praile.florian@gmail.com>
2018-09-12 10:41:53 +0200

    map annotation

--- MapKitDemo/ViewController.swift
+++ MapKitDemo/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapview: MKMapView!
+    
+    let geoCoder = CLGeocoder()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mapview.delegate = self
+        
+        self.addMapAnnotation(fromCoordinate: CLLocationCoordinate2D(latitude: 50.476925, longitude: 4.868806))
+        
+        
+        let address = "Rue Royale 67, 1000 Bruxelles, Belgium"
+        
+        
+        geoCoder.geocodeAddressString(address) { (placemarks, error) in
+            if let error = error{
+                print(error)
+            }else if let placemark = placemarks?.first, let coordinate = placemark.location?.coordinate{
+                print(placemark)
+                self.addMapAnnotation(fromCoordinate: coordinate)
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +44,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func addMapAnnotation(fromCoordinate coordinate: CLLocationCoordinate2D){
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        mapview.addAnnotation(annotation)
+    }
 
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        guard let coordinate = view.annotation?.coordinate else{
+            return
+        }
+     
+        let location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        geoCoder.reverseGeocodeLocation(location) { (placemarks, error) in
+            if let error = error{
+                print(error)
+            }else if let placemark = placemarks?.first{
+                print(placemark)
+            }
+        }
+        
+    }
+    
+    
+    
+    
 }
 
